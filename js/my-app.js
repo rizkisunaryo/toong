8396cfb3ea0f729d389010e17a2f9466545fdc75
rizkisunaryo <rizkisunaryo@gmail.com>
2015-12-21 22:24:52 +0700

    take photo

--- js/my-app.js
+++ js/my-app.js
@@ -1,11 +1,17 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    swipePanel: 'left',
+    modalTitle: 'Toong',
+    // ... other parameters
+});
 
 // Export selectors engine
 var $$ = Dom7;
 
 // Add views
-var view1 = myApp.addView('#view-1');
+var view1 = myApp.addView('#view-1', {
+	dynamicNavbar: true,
+});
 var view2 = myApp.addView('#view-2', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
